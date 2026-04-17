45dcf9b3b06cb4423a3b14495406ec961208bc16
ABaker97 <35184574+ABaker97@users.noreply.github.com>
2026-04-17 22:47:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function WeddingWebsitePreview() {
   const weddingDate = new Date("2026-07-17T18:00:00");
